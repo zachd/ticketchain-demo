75550be56faf6aaaadf2c2a9a4ca116bbb1df158
Zachary <hello@zach.ie>
2017-02-09 12:20:03 +0000

    Add more tickets to Ed Sheeran / RHCP

--- demo/contracts/TicketChain.sol
+++ demo/contracts/TicketChain.sol
@@ -32,8 +32,8 @@ contract TicketChain {
     mapping(uint => Ticket) tickets;
 
     function TicketChain() {
-        newEvent("Ed Sheeran", 750000000000000000, 1);
-        newEvent("Red Hot Chili Peppers", 650000000000000000, 20);
+        newEvent("Ed Sheeran", 750000000000000000, 20);
+        newEvent("Red Hot Chili Peppers", 650000000000000000, 50);
         newEvent("Cirque Du Soleil", 450000000000000000, 100);
         newEvent("Hans Zimmer", 850000000000000000, 150);
     }
